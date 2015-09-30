e128de1773214e6c658002a5c30f96b9562ba441
luke7515 <luke7515@gmail.com>
2015-09-30 10:31:44 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+    def new
+    end
+    
+    def temp
+    end
+    
+    def sports
+    end
+    
+end
